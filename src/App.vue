6cbd478ee764bed6dbbe16538bcf8c47dbb29a45
gogogoghost <491511934@qq.com>
2019-02-10 18:27:52 +0800

    db

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
       }
     },
     created(){
-
+      this.db.read();
     },
     methods:{
 
@@ -28,6 +28,9 @@
           onerror() {
             //console.log(arguments)
           },
+          log(){
+
+          },
           wdosboxUrl:'/static/libs/js-dos/wdosbox.js'
         }).ready((fs, main) => {
           main([]).then(dos=>{
@@ -40,14 +43,40 @@
                 }
               })
             }
-            return;
+            if(false){
+              this.db.eachFile((name,buffer)=>{
+                dos.api.FS.mkdir('/dir')
+                dos.api.FS.writeFile('/dir/'+name,new Uint8Array(buffer))
+              })
+              return;
+            }
+
             fs.extract("/static/games/pal.zip").then(() => {
               dos.exec(['rescan']).then(res=>{
                 console.log('done')
-                console.log(dos.api.FS.readdir('/'))
-                let data=dos.api.FS.readFile('/PAL.EXE')
+                console.log(dos.api.FS.readdir('/tmp'))
+                console.log(dos.api.FS);
+                let n='/PAL.EXE'
+                console.log(dos.api.FS.isDir(dos.api.FS.stat('/tmp').mode))
+                /*try{
+                  if(dos.api.FS.readFile('/no')){
+                    console.log('true');
+                  }else{
+                    console.log('false')
+                  }
+                }catch (err) {
+                  console.log(err)
+                }*/
+
+                return;
+                let name='/PAL!.EXE'
+                let data=dos.api.FS.readFile(name)
                 console.log(data);
-                dos.api.FS.writeFile('/test.exe',data)
+                this.db.clear();
+                this.db.add(name,data.buffer);
+                this.db.save();
+                //this.db.add('pal.exe',data.arraybuffer);
+                //dos.api.FS.writeFile('/test.exe',data)
               })
               //shell(['pal!.exe']);
             }).catch((err)=>{
--- src/libs/db.js
+++ src/libs/db.js
@@ -3,14 +3,71 @@ import Dexie from 'dexie';
 const dbName='dosbox';
 
 export default class GameDB {
+
+  //read database
   constructor(gameName){
     this.db=new Dexie(dbName);
     this.db.version(1).stores({
-      gameSave:'name,files'
+      gameSave:'name'
     });
-    this.files=this.db.gameSave.get(gameName);
-    for(let key in this.files){
-      console.log(key,this.files[key])
+    this.gameName=gameName;
+  }
+
+  /*
+  * read files
+  * gameData:{fileName:fileData(arraybuffer)}
+  * */
+  read(){
+    return new Promise((resolve,reject)=>{
+      this.db.gameSave.where('name').equals(this.gameName).first().then(res=>{
+        this.gameData=res?res.files:{};
+        resolve(this.gameData);
+      }).catch(err=>{
+        reject(err);
+      });
+    })
+  }
+
+  /*
+  * save files
+  * */
+  save(){
+    return new Promise((resolve,reject)=>{
+      this.db.gameSave.put({
+        name:this.gameName,
+        files:this.gameData
+      }).then(res=>{
+        console.log(res);
+        resolve();
+      }).catch(err=>{
+        console.log(err);
+        reject();
+      })
+    })
+  }
+
+  /*
+  * clear all files
+  * */
+  clear(){
+    this.gameData={};
+  }
+
+  /*
+  * add a file
+  * */
+  add(name,buffer){
+    this.gameData[name]=buffer;
+  }
+
+  /*
+  * foreach
+  * */
+  eachFile(cb){
+    for(let key in this.gameData){
+      cb(key,this.gameData[key]);
     }
   }
 }
+
+

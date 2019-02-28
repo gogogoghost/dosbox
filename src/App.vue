ff0a6597e485e3b4796e372afed08435de9a02a3
gogogoghost <491511934@qq.com>
2019-02-28 17:31:27 +0800

    基本布局

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <template>
-  <el-container>
-    <el-header>
+  <div class="wrapper">
+    <el-row class="full-height">
+      <el-col :xs="24" :md="18" :xl="16" class="container">
 
-    </el-header>
-    <el-main>
-      <div>
-        <canvas ref="main"></canvas>
-      </div>
-    </el-main>
-  </el-container>
+        <el-row :gutter="20" class="main-box">
+          <el-col :xs="24" :md="16" :xl="16">
+            <canvas ref="main"></canvas>
+          </el-col>
+          <el-col :xs="24" :md="8" :xl="8">
+            <div class="title">
+              等待选择游戏
+            </div>
+            <el-row :gutter="10">
+              <el-col :xs="8" :sm="6" :md="12" :xl="8">
+                <el-button @click="fullGame">全屏游戏</el-button>
+              </el-col>
+              <el-col :xs="8" :sm="6" :md="12" :xl="8">
+                <el-button>重新加载</el-button>
+              </el-col>
+              <el-col :xs="8" :sm="6" :md="12" :xl="8">
+                <el-button>保存存档</el-button>
+              </el-col>
+              <el-col :xs="8" :sm="6" :md="12" :xl="8">
+                <el-button>加载存档</el-button>
+              </el-col>
+              <el-col :xs="8" :sm="6" :md="12" :xl="8">
+                <el-button>导出存档</el-button>
+              </el-col>
+              <el-col :xs="8" :sm="6" :md="12" :xl="8">
+                <el-button>导入存档</el-button>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -21,20 +49,54 @@
       }
     },
     methods:{
-
+      fullGame(){
+        this.$refs.main.requestFullscreen();
+      }
     },
     mounted() {
       this.$nextTick(()=>{
-        dosbox(this.$refs.main,gameConfig.list[0]).then(()=>{
-        });
+        /*dosbox(this.$refs.main,gameConfig.list[0]).then(()=>{
+        });*/
       })
     }
   }
 </script>
-
-<style lang="scss" scoped>
-  canvas{
-    width:100%;
-    height:calc(50%);
+<style lang="scss">
+  .main-box{
+    .el-button{
+      margin-left:0 !important;
+      margin-right:10px !important;
+      margin-bottom:10px;
+    }
   }
+</style>
+<style lang="scss" scoped>
+  .main-box{
+    padding:20px;
+    canvas{
+      width:100%;
+      height:calc(50%);
+      background-color: black;
+    }
+    .title{
+      text-align: center;
+      font-size:20px;
+      margin-bottom:20px;
+    }
+    button{
+      width:100%;
+    }
+  }
+  .wrapper{
+    position: absolute;
+    width:100%;
+    height:100%;
+  }
+  .container{
+    margin:0 auto;
+    float: none;
+    height:100%;
+    background-color: #DCDFE6;
+  }
+
 </style>

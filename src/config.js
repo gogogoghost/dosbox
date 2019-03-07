64245c2cff3a844e74ba01e7301535434dceb4e5
gogogoghost <491511934@qq.com>
2019-03-07 10:44:48 +0800

    build

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   //staticBaseUrl:process.env.NODE_ENV=='production'?'http://static.gogogoghost.site/dosbox/':'http://static.gogogoghost.site/dosbox/'
-  staticBaseUrl:'http://static.gogogoghost.site/dosbox/'
+  staticBaseUrl:'https://static.gogogoghost.site/dosbox/'
 }

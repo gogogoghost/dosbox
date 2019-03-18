6fcfad936ba1e20c1bbab8a080b95d97880d9107
gogogoghost <491511934@qq.com>
2019-03-18 22:04:38 +0800

    dist

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  //staticBaseUrl:process.env.NODE_ENV=='production'?'http://static.gogogoghost.site/dosbox/':'http://static.gogogoghost.site/dosbox/'
-  staticBaseUrl:'https://static.gogogoghost.site/dosbox/'
-}

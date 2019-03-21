825dd0841812b25780dc4bd20b323ff4c040ab78
gogogoghost <491511934@qq.com>
2019-03-21 17:43:56 +0800

    build

--- src/libs/download.js
+++
@@ -1,9 +0,0 @@
-export default function(name,blob){
-  let a=document.createElement('a');
-  a.href=URL.createObjectURL(blob);
-  a.download=name;
-  a.style.display='none';
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-}
--- src/libs/game.config.js
+++
@@ -1,6 +0,0 @@
-import config from '../config'
-export default {
-  gameBaseUrl:config.staticBaseUrl+'games/',
-  posterBaseUrl:config.staticBaseUrl+'posters/',
-  list:require('./game')
-}
